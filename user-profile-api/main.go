9ff39ea24e05e09bbc8011e264c543d0193a0f1c
Edward Rees <erees.hk@gmail.com>
2024-02-07 19:36:12 -0800

    [feat] updated port to use env variables

--- user-profile-api/main.go
+++ user-profile-api/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+  "fmt"
+  "log"
+  "strconv"
   "user-profile/api/database"
   "user-profile/api/router"
+  "user-profile/api/config"
   "github.com/gofiber/fiber/v2"
   "github.com/gofiber/fiber/v2/middleware/cors"
   "github.com/gofiber/fiber/v2/middleware/logger"
@@ -24,5 +28,15 @@ func main() {
     return c.SendStatus(404)
   })
 
-  app.Listen(":8080")
+  p := config.Config("PORT")
+  port, err := strconv.ParseUint(p, 10, 32)
+
+  if err != nil {
+    fmt.Println("No port given, defaulting to 8080")
+    port = 8080
+  }
+
+
+  log.Printf("Running at port %d\n", port)
+  app.Listen(fmt.Sprintf(":%d", port))
 }
